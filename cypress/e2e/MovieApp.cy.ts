c4256d1b5bb25765d52b292552894bcdfecdfef4
ymer94 <ymer_94@hotmail.com>
2025-09-09 07:47:04 +0200

    Added test for live and stubbed searches.
    Changes to be committed:
    	modified:   cypress/e2e/MovieApp.cy.ts

--- cypress/e2e/MovieApp.cy.ts
+++ cypress/e2e/MovieApp.cy.ts
@@ -1,12 +1,19 @@
 import Helpers from '../utilities/Helpers'
 
-const ouml = Helpers.HTMLEntity.decode('&ouml;'), defaultIntercept = () => {
-    cy.intercept('GET', 'http://omdbapi.com/*', {
+enum ID {
+    SEARCH_FORM = 'searchForm',
+    SEARCH_TEXT = 'searchText',
+    SEARCH_BUTTON = 'search',
+    MOVIE_CONTAINER = 'movie-container'
+}
+
+const ouml = Helpers.HTMLEntity.decode('&ouml;'), API_URL = 'http://omdbapi.com/*', defaultIntercept = () => {
+    cy.intercept('GET', API_URL, {
         statusCode: 200  
     })    
 }, searchForLotR = () => {
-    cy.get('#searchText').type('Lord of the Rings')
-    cy.get('#search').click()
+    cy.get(`#${ ID.SEARCH_TEXT }`).type('Lord of the Rings')
+    cy.get(`#${ ID.SEARCH_BUTTON }`).click()
 }
 
 describe('Movies App', () => {
@@ -18,20 +25,20 @@ describe('Movies App', () => {
         cy.url().should('equal', 'http://localhost:5173/')
     })
 
-    it('should have one form with id "searchForm"', () => {
-        cy.get('form').should('have.length', 1).and('not.have.length.lessThan', 1).and('not.have.length.greaterThan', 1)  .and('have.id', 'searchForm')
+    it(`should have one form with id "${ ID.SEARCH_FORM }"`, () => {
+        cy.get('form').should('have.length', 1).and('not.have.length.lessThan', 1).and('not.have.length.greaterThan', 1).and('have.id', ID.SEARCH_FORM)
     })
 
     it('should have one input in the form of type text with id "searchText" and placeholder text without value', () => {
-        cy.get('form input[type="text"]').should('have.length', 1).and('have.id', 'searchText').and('have.value', '').and('have.attr', 'placeholder').and('match', /[\w-~,."'()[\]{}]+/gi)
+        cy.get(`#${ ID.SEARCH_FORM } input[type="text"]`).should('have.length', 1).and('have.id', ID.SEARCH_TEXT).and('have.value', '').and('have.attr', 'placeholder').and('match', /[\w-~,."'()[\]{}]+/gi)
     })
 
     it(`should have one button in the form of type submit with id "search" and text "S${ ouml }k"`, () => {
-        cy.get('form button').should('have.length', 1).and('have.id', 'search').and('have.text', `S${ ouml }k`)
+        cy.get(`#${ ID.SEARCH_FORM } button`).should('have.length', 1).and('have.id', ID.SEARCH_BUTTON).and('have.text', `S${ ouml }k`)
     })
 
     it('should have a container for movies search results with id "movie-container" that is empty', () => {
-        cy.get('#movie-container').children().should('have.length', 0)
+        cy.get(`#${ ID.MOVIE_CONTAINER }`).children().should('have.length', 0)
     })
 
     it('should search for movies but find none and display a message accordingly', () => {
@@ -39,6 +46,25 @@ describe('Movies App', () => {
 
         searchForLotR()
 
-        cy.get('#movie-container').should('have.length', 1).find('p').should('have.length', 1).and('have.text', `Inga s${ ouml }kresultat att visa`)
+        cy.get(`#${ ID.MOVIE_CONTAINER }`).children().should('have.length', 1).parent().children('p').should('have.length', 1).and('have.text', `Inga s${ ouml }kresultat att visa`)
     })
+
+    it('should search for movies and displa 10 movie cards', () => {
+        cy.intercept('GET', API_URL, {
+            statusCode: 200,
+            fixture: 'Lord_of_the_Rings.json'
+        })
+
+        searchForLotR()
+
+        cy.get(`#${ ID.MOVIE_CONTAINER }`).children().should('have.length', 10)
+    })
+
+    // Live tests to be uncommented/turned on later.
+
+    /* it('should search for movies and display 10 movie cards', () => {
+        searchForLotR()
+
+        cy.get(`#${ ID.MOVIE_CONTAINER }`).children().should('have.length', 10)
+    }) */
 })
