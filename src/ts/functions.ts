8ef62dd10dc5d90c93562f0c7a95d73316a9feb6
ymer94 <ymer_94@hotmail.com>
2025-09-10 15:50:53 +0200

    Updated: Removed explicit type annotations since TypeScript can infer them.
    Changes to be committed:
    	modified:   src/ts/functions.ts

--- src/ts/functions.ts
+++ src/ts/functions.ts
@@ -1,3 +1,3 @@
 import { IMovie } from './models/Movie'
 
-export const movieSort = (movies: IMovie[], asc: boolean = true) => movies.sort((a: IMovie, b: IMovie) => asc ? a.Title.localeCompare(b.Title) : b.Title.localeCompare(a.Title))
+export const movieSort = (movies: IMovie[], asc: boolean = true) => movies.sort((a, b) => asc ? a.Title.localeCompare(b.Title) : b.Title.localeCompare(a.Title))
