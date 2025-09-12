ea558d7609d61c73c81744c5c2c1ebdfd9c641f9
ymer94 <ymer_94@hotmail.com>
2025-09-12 08:25:23 +0200

    Updated: refactored file to be smaller & follow indentation standard
    Indentation stanndard: tab = 4 spaces wide
    
    Also removed unused svg.
    
    Changes to be committed:
    	modified:   src/ts/movieApp.ts
    	deleted:    src/typescript.svg

--- src/ts/movieApp.ts
+++ src/ts/movieApp.ts
@@ -6,78 +6,61 @@ const movieContainerID = 'movie-container'
 
 let movies: IMovie[] = [], sort = true
 
-export const init = () => {
-  const form = document.getElementById('searchForm') as HTMLFormElement, sortID = 'sort', sortTag = document.createElement('select')
+	export const init = () => {
+	const form = document.getElementById('searchForm') as HTMLFormElement, sortID = 'sort', sortTag = document.createElement('select')
 
-  Object.assign(sortTag, {
-    id: sortID,
-    innerHTML: `
-      <option value="asc">A-&Ouml;</option>
-      <option value="desc">&Ouml;-A</option>
-    `
-  })
+	Object.assign(sortTag, {
+		id: sortID,
+		innerHTML: `
+		<option value="asc">A-&Ouml;</option>
+		<option value="desc">&Ouml;-A</option>
+		`
+	})
 
-  sortTag.addEventListener('change', (e: Event) => {
-    sort = (e.target as HTMLSelectElement).value === 'asc'
-    
-    createHtml(movies, Object.assign(document.getElementById(movieContainerID) as HTMLDivElement, {
-      innerHTML: ''
-    }))
-  })
+	sortTag.addEventListener('change', (e: Event) => {
+		sort = (e.target as HTMLSelectElement).value === 'asc'
+		createHtml(movies, Object.assign(document.getElementById(movieContainerID) as HTMLDivElement, {innerHTML: '' }))
+	})
 
-  form.addEventListener('submit', (e: SubmitEvent) => {
-    e.preventDefault()
-    handleSubmit()
-  })
+	form.addEventListener('submit', (e: SubmitEvent) => {
+		e.preventDefault()
+		handleSubmit()
+	})
 
-  form.append(Object.assign(document.createElement('label'), {
-    htmlFor: sortID,
-    innerHTML: 'Sortera: '
-  }), sortTag)
+	form.append(Object.assign(document.createElement('label'), { htmlFor: sortID, innerHTML: 'Sortera: ' }), sortTag)
 }
 
 export async function handleSubmit() {
-  let searchText = (document.getElementById('searchText') as HTMLInputElement).value, container = document.getElementById(movieContainerID) as HTMLDivElement
+	let searchText = (document.getElementById('searchText') as HTMLInputElement).value, container = document.getElementById(movieContainerID) as HTMLDivElement
 
-  container.innerHTML = ''
+	container.innerHTML = ''
 
-  try {
-    movies = await getData(searchText)
+	try {
+		movies = await getData(searchText)
 
-    if (movies.length > 0) {
-      createHtml(movies, container)
-    } else {
-      displayNoResult(container)
-    }
-  } catch {
-    displayNoResult(container)
-  }
+		if (movies.length > 0) {
+		createHtml(movies, container)
+		} else {
+		displayNoResult(container)
+		}
+	} catch {
+		displayNoResult(container)
+	}
 }
 
 export const createHtml = (movies: IMovie[], container: HTMLDivElement) => {
-  movieSort(movies, sort)
+	movieSort(movies, sort)
 
-  for (let i = 0; i < movies.length; i++) {
-    let movie = document.createElement('div')
-    let title = document.createElement('h3')
-    let img = document.createElement('img')
+	movies.forEach(m => {
+		const movie = document.createElement('div')
 
-    movie.classList.add('movie')
-    title.innerHTML = movies[i].Title
-    img.src = movies[i].Poster
-    img.alt = movies[i].Title
+		movie.classList.add('movie')
 
-    movie.appendChild(title)
-    movie.appendChild(img)
-
-    container.appendChild(movie)
-  }
+		movie.append(Object.assign(document.createElement('h3'), { innerHTML: m.Title }), Object.assign(document.createElement('img'), { src: m.Poster, alt: m.Title }))
+		container.append(movie)
+	})
 }
 
 export const displayNoResult = (container: HTMLDivElement) => {
-  let noMessage = document.createElement('p')
-
-  noMessage.innerHTML = 'Inga s&ouml;kresultat att visa'
-
-  container.appendChild(noMessage)
+  	container.append(Object.assign(document.createElement('p'), { innerHTML: 'Inga s&ouml;kresultat att visa' }))
 }
